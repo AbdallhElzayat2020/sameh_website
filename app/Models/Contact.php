ec5e1fbf5d84418f636c83927bcaa84849a6a6d1
Ahmed M. Sadik <ahmed.sadik.dev@gmail.com>
2025-11-18 09:34:26 +0200

    Add table property

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Contact extends Model
 {
+    protected $table = 'contact_messages';
+
     protected $fillable = [
         'name',
         'email',
